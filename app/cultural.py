de330c20a626919011a247397502517f57b96a91
alaypatel07 <alay1431@gmail.com>
2015-04-04 11:42:57 +0530

    Cultural Endpoint added

---
+++ app/cultural.py
@@ -0,0 +1,18 @@
+__author__ = 'alay'
+
+
+from app.basehandler import BaseHandler
+from app.asynchandledoc import AsyncHandleDoc
+from tornado.gen import coroutine
+
+
+class CulturalHandler(BaseHandler):
+
+    @coroutine
+    def get(self, *args, **kwargs):
+        client = AsyncHandleDoc('cultural')
+        client.get_query()
+        yield client.run_query()
+        client.get_data()
+        self.message = client.doc
+        self.send_error(200)
